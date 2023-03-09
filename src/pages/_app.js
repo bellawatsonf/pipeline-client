4ad34ec858334c5e742c530a8388c40dae88ad5c
bellawatsonf <bellawatsonf@gmail.com>
2023-03-09 20:55:39 +0700

    client

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,23 +2,23 @@ import FullLayout from "../layouts/FullLayout";
 import Head from "next/head";
 import "../styles/style.scss";
 import "../styles/global.css";
-import { RouteGuard } from "../../RouteGuard";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react"
+// import { RouteGuard } from "../../RouteGuard";
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const [token, setToken] = useState("")
+  // const router = useRouter();
+  // const [token, setToken] = useState("");
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      // Perform localStorage action
-      setToken(localStorage.getItem("token"));
-    }
-    console.log(token, "props");
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     // Perform localStorage action
+  //     setToken(localStorage.getItem("token"));
+  //   }
+  //   console.log(token, "props");
 
-    if (!token) router.push("/login");
-  }, [token]);
+  //   if (!token) router.push("/login");
+  // }, [token]);
 
   return (
     <>
