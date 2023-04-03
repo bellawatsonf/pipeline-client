1965d81a3ccbe64d60d5646bc07ef2e753fde799
bellawatsonf <bellawatsonf@gmail.com>
2023-04-04 01:09:41 +0700

    edit report

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -88,21 +88,21 @@ export default function Home() {
     console.log(params, "resmasukfetch");
     let link = "";
     if (params.search !== undefined) {
-      link = `http://localhost:3000/pipeline-user?page=${params.page}&size=100&nama_nasabah=${params.search}`;
+      link = `https://server-pipeline.herokuapp.com/pipeline-user?page=${params.page}&size=100&nama_nasabah=${params.search}`;
     } else {
-      link = `http://localhost:3000/pipeline-user?page=${params.page}&size=100`;
+      link = `https://server-pipeline.herokuapp.com/pipeline-user?page=${params.page}&size=100`;
     }
 
     let linkAdmin = "";
     if (params.search !== undefined) {
-      linkAdmin = `http://localhost:3000/pipeline?page=${params.page}&size=100&nama_nasabah=${params.search}`;
+      linkAdmin = `https://server-pipeline.herokuapp.com/pipeline?page=${params.page}&size=100&nama_nasabah=${params.search}`;
     } else {
-      linkAdmin = `http://localhost:3000/pipeline?page=${params.page}&size=100`;
+      linkAdmin = `https://server-pipeline.herokuapp.com/pipeline?page=${params.page}&size=100`;
     }
 
     axios({
       method: "get",
-      url: `http://localhost:3000/pipeline?page=${params.page}&size=100`,
+      url: `https://server-pipeline.herokuapp.com/pipeline?page=${params.page}&size=100`,
       // levelUser === "admin" || levelUser === "super admin" ? linkAdmin : link,
       headers: {
         token: localStorage.getItem("token"),
@@ -138,7 +138,7 @@ export default function Home() {
   async function fetchpipelinegroup() {
     axios({
       method: "get",
-      url: `http://localhost:3000/pipeline-group`,
+      url: `https://server-pipeline.herokuapp.com/pipeline-group`,
       // levelUser === "admin" || levelUser === "super admin" ? linkAdmin : link,
       headers: {
         token: localStorage.getItem("token"),
